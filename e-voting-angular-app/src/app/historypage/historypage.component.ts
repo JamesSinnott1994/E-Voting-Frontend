cb7cc106111dd9e91b84180430480de38dc28502
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2024-11-26 16:40:43 +0000

    [ADD] E Voting History component created

---
+++ e-voting-angular-app/src/app/historypage/historypage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historypage',
+  imports: [],
+  templateUrl: './historypage.component.html',
+  styleUrl: './historypage.component.css'
+})
+export class HistorypageComponent {
+
+}
