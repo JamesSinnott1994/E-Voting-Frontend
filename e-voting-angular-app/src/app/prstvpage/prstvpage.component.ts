6bbc14f5085303c9baada6e2444ddd811225183c
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2024-11-30 18:38:11 +0000

    [ADD] Button link to PR-STV page working

--- e-voting-angular-app/src/app/prstvpage/prstvpage.component.ts
+++ e-voting-angular-app/src/app/prstvpage/prstvpage.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-prstvpage',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './prstvpage.component.html',
   styleUrl: './prstvpage.component.css'
 })
