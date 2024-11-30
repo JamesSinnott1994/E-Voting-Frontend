fc5b734fd75531ccb473a4f0a510047137d55ee2
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2024-11-30 16:45:59 +0000

    [ADD] PRSTV Page

---
+++ e-voting-angular-app/src/app/prstvpage/prstvpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prstvpage',
+  imports: [],
+  templateUrl: './prstvpage.component.html',
+  styleUrl: './prstvpage.component.css'
+})
+export class PrstvpageComponent {
+
+}
